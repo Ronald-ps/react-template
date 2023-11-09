5f10ea9c3525e328f03bad3fb5d1a79aa7573057
Ronald Seabra <git.ronaldseabra@gmail.com>
2023-11-09 18:35:22 -0300

    feat: mais uma tab de navegação

--- src/pages/DefaultPagesContainer.tsx
+++ src/pages/DefaultPagesContainer.tsx
@@ -1,12 +1,22 @@
 import { useDisclosure } from "@mantine/hooks";
-import { AppShell, Burger, Container, Group, NavLink, Skeleton } from "@mantine/core";
+import {
+  AppShell,
+  Box,
+  Burger,
+  Container,
+  Group,
+  NavLink,
+  Skeleton,
+  Text,
+  Title,
+} from "@mantine/core";
 import { IconAdOff } from "@tabler/icons-react";
 import { Outlet } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 export const DefaultPagesContainer = () => {
   const [opened, { toggle }] = useDisclosure();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <AppShell
@@ -17,11 +27,14 @@ export const DefaultPagesContainer = () => {
       <AppShell.Header>
         <Group h="100%" px="md">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <IconAdOff />
+          <Box>
+            <Text fw={600}>Brag App</Text>
+          </Box>
         </Group>
       </AppShell.Header>
       <AppShell.Navbar p="md">
-        <NavLink label="Página inicial" onClick={() => navigate("/")}/>
+        <NavLink label="Página inicial" onClick={() => navigate("/")} />
+        <NavLink label="Documentos" onClick={() => navigate("/")} />
       </AppShell.Navbar>
       <AppShell.Main>
         <Outlet />
